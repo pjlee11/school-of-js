3a3f6148f0abb32c3958a1eadae2254b73329aef
pjlee11 <pjlee112@hotmail.co.uk>
2021-01-19 14:57:15 +0000

    package setup + async-js

---
+++ async-js/callback-example.js
@@ -0,0 +1,25 @@
+const func = (foo, callback) => {
+  console.log('inside func before if');
+  if (foo === 'shouldCallback') {
+    setTimeout(callback, 3000);
+  } else {
+    console.log('should not callback()');
+  }
+  console.log('inside func after if');
+};
+
+func('shouldCallback', () => console.log('execute callback'));
+
+// func('shouldNotCallback', () => console.log('execute callback'));
+
+// const func = (foo, callback) => {
+//   console.log('inside func before Promise');
+//   return new Promise((resolve) => {
+//     if (foo !== 'shouldCallback') {
+//       rejects(() => console.log('should not callback()'));
+//     }
+//     setTimeout(() => resolve(callback), 3000);
+//   }
+// };
+
+// func('shouldCallback', () => console.log('execute callback'));
---
+++ yarn-basics/yarn-basics.md
@@ -0,0 +1,18 @@
+## Yarn basics 
+
+Some basic yarn commands that are worth knowing! 
+
+### Commands
+
+Install the dependencies of this package: 
+`yarn` which is the same as `yarn install`
+
+Add a package to the `dependencies`:
+`yarn add ${PACKAGE_NAME}`
+
+Add a package to the `devDependencies`:
+`yarn add -D ${PACKAGE_NAME}`
+
+See why a package is installed (including which versions) in node_modules/:
+`yarn why ${PACKAGE_NAME}`
+
