7bfc2863eb5ac34972e6331236ab2991de1a3d79
pjlee11 <pjlee112@hotmail.co.uk>
2021-03-23 15:09:15 +0000

    WIP

--- types-of-apps/client-side-rendering/webpack.client.js
+++ types-of-apps/client-side-rendering/webpack.client.js
@@ -3,11 +3,12 @@
  * We then add these bundles to the HTML template as <script> tags.
  */
 const path = require('path');
+const OptimizePlugin = require('optimize-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 
 module.exports = {
-  entry: './src/client/index.js', // the file webpack should start from
+  entry: './src/client/index.jsx', // the file webpack should start from
   mode: process.env.NODE_ENV || 'development', // default as development mode
   output: {
     path: path.resolve(__dirname, 'build/client'), // output the bundles to build/client
@@ -15,17 +16,18 @@ module.exports = {
   },
   plugins: [
     // Generates an `index.html` file with the <script> injected.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve('public/index.html'),
-    }),
+    // new HtmlWebpackPlugin({
+    //   inject: true,
+    //   template: path.resolve('public/index.html'),
+    // }),
     // Inlines chunks with `client-app` in the name
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/client-app/]),
+    // new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/client-app/]),
+    new OptimizePlugin()
   ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/, // match all js and jsx files
+        test: /\.jsx$/, // match all js and jsx files
         loader: 'babel-loader', // run babel on them
         exclude: /node_modules/, // don't babel the node_modules/
       }
